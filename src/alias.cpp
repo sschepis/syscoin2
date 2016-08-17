8b37236a76204313ff23ffe7fb06649c45f89cb0
sidhujag <sidhujag@hotmail.com>
2016-08-17 12:21:31 -0700

    Revert "typo"
    This reverts commit 052352e7460d4d6e8a01fca7f9cc925e812d6630.

--- src/alias.cpp
+++ src/alias.cpp
@@ -1622,7 +1622,7 @@ UniValue aliasupdate(const UniValue& params, bool fHelp) {
 	
 	theAlias.nRenewal = nRenewal;
 	theAlias.safeSearch = strSafeSearch == "Yes"? true: false;
-	CPubKey currentKey(theAlias.vchPubKey);
+	CPubKey currentKey(vchPubKeyByte);
 	scriptPubKeyOrig = GetScriptForDestination(currentKey.GetID());
 
 	const vector<unsigned char> &data = theAlias.Serialize();
