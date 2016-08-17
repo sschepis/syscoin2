31b8534fcdbe00081bd0814b2f69872326499987
sidhujag <sidhujag@hotmail.com>
2016-08-17 14:21:56 -0700

    private profile

--- src/qt/editaliasdialog.cpp
+++ src/qt/editaliasdialog.cpp
@@ -122,7 +122,7 @@ bool EditAliasDialog::saveCurrentRow()
 		strMethod = string("aliasnew");
         params.push_back(ui->aliasEdit->text().trimmed().toStdString());
 		params.push_back(ui->nameEdit->toPlainText().toStdString());
-		params.push_back("");
+		params.push_back(ui->privateEdit->toPlainText().toStdString());
 		params.push_back(ui->safeSearchEdit->currentText().toStdString());
 		params.push_back(ui->expiryEdit->itemData(ui->expiryEdit->currentIndex()).toString().toStdString());
 		try {
@@ -157,7 +157,7 @@ bool EditAliasDialog::saveCurrentRow()
 			strMethod = string("aliasupdate");
 			params.push_back(ui->aliasEdit->text().toStdString());
 			params.push_back(ui->nameEdit->toPlainText().toStdString());
-			params.push_back("");
+			params.push_back(ui->privateEdit->toPlainText().toStdString());
 			params.push_back(ui->safeSearchEdit->currentText().toStdString());	
 			params.push_back("");
 			params.push_back(ui->expiryEdit->itemData(ui->expiryEdit->currentIndex()).toString().toStdString());
@@ -193,7 +193,7 @@ bool EditAliasDialog::saveCurrentRow()
 			strMethod = string("aliasupdate");
 			params.push_back(ui->aliasEdit->text().toStdString());
 			params.push_back(ui->nameEdit->toPlainText().toStdString());
-			params.push_back("");
+			params.push_back(ui->privateEdit->toPlainText().toStdString());
 			params.push_back(ui->safeSearchEdit->currentText().toStdString());
 			params.push_back(ui->transferEdit->text().toStdString());
 			params.push_back(ui->expiryEdit->itemData(ui->expiryEdit->currentIndex()).toString().toStdString());
