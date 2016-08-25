503c2119d0c71db11b4c30eb74c4d29d0a55500a
sidhujag <sidhujag@hotmail.com>
2016-08-25 11:35:05 -0700

    try to fix import alias

--- src/alias.cpp
+++ src/alias.cpp
@@ -2203,7 +2203,8 @@ UniValue importalias(const UniValue& params, bool fHelp) {
             CBlock block;
             if(ReadBlockFromDisk(block, chainActive[theAlias.nHeight], Params().GetConsensus()))
 			{
-				pwalletMain->SyncTransaction(tx, &block);
+				LOCK2(cs_main, cs_wallet);
+				AddToWalletIfInvolvingMe(tx, &block, true))
 			}
 		}
 	}
