7c63b6f8d4047e50cd5dd05e3a6fde0fd7b8f615
sidhujag <sidhujag@hotmail.com>
2016-09-06 10:24:30 -0700

    remove translation

--- src/qt/offeracceptinfodialog.cpp
+++ src/qt/offeracceptinfodialog.cpp
@@ -202,8 +202,8 @@ bool OfferAcceptInfoDialog::lookup()
 						ui->btctxidEdit->setText(btctxidStr);
 					}
 				}
-				ui->priceEdit->setText(tr("%1 %2").arg(QString::fromStdString(find_value(acceptObj "price").get_str())).arg(currencyStr));
-				ui->totalEdit->setText(tr("%1 %2").arg(QString::fromStdString(find_value(acceptObj, "total").get_str())).arg(currencyStr));
+				ui->priceEdit->setText(QString("%1 %2").arg(QString::fromStdString(find_value(acceptObj "price").get_str())).arg(currencyStr));
+				ui->totalEdit->setText(QString("%1 %2").arg(QString::fromStdString(find_value(acceptObj, "total").get_str())).arg(currencyStr));
 				ui->discountEdit->setText(QString::fromStdString(find_value(acceptObj, "offer_discount_percentage").get_str()));
 				ui->paidEdit->setText(QString::fromStdString(find_value(acceptObj, "paid").get_str()));
 				QString buyerStr = QString::fromStdString(find_value(acceptObj.get_obj(), "buyer").get_str());
