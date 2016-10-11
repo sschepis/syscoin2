927579090f38daeaa9172ddd36e1f9f78854b3ae
sidhujag <sidhujag@hotmail.com>
2016-10-11 14:03:01 -0700

    typo

--- src/script/standard.cpp
+++ src/script/standard.cpp
@@ -74,7 +74,7 @@ bool Solver(const CScript& scriptPubKeyIn, txnouttype& typeRet, vector<vector<un
         vector<unsigned char> hashBytes(scriptPubKey.begin()+2, scriptPubKey.begin()+22);
         vSolutionsRet.push_back(hashBytes);
 		// SYSCOIN if multisig
-		CScriptID innerID(uint160(hashBytes));
+		CScriptID innerID(uint160(vSolutionsRet[0]));
 		CSyscoinAddress syscoinAddress(innerID);
 		syscoinAddress = CSyscoinAddress(syscoinAddress.ToString());
 		if(syscoinAddress.nRequiredSigs > 1 && syscoinAddress.vchPubKeys.size() >= syscoinAddress.nRequiredSigs)
