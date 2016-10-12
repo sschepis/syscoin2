ffbac11f1a5d7b026423a2b1d62fe31a1df1fdac
sidhujag <sidhujag@hotmail.com>
2016-10-12 13:36:25 -0700

    typo

--- src/script/ismine.cpp
+++ src/script/ismine.cpp
@@ -73,7 +73,7 @@ isminetype IsMine(const CKeyStore &keystore, const CScript& scriptPubKey)
                 return ret;
         }
 		// SYSCOIN if multisig
-		/*vector<valtype> keys;
+		vector<valtype> keys;
 		CSyscoinAddress syscoinAddress(scriptID);
 		syscoinAddress = CSyscoinAddress(syscoinAddress.ToString());
 		if(syscoinAddress.vchPubKeys.size() > 1)
@@ -87,7 +87,7 @@ isminetype IsMine(const CKeyStore &keystore, const CScript& scriptPubKey)
 				return ISMINE_SPENDABLE;
 			else if(nNumKeysFound > 0)
 				return ISMINE_WATCH_SOLVABLE;	
-		}*/
+		}
         break;
     }
     case TX_WITNESS_V0_SCRIPTHASH:
