76da01e06afb0b76b3582dd04ea50d45ada963a3
sidhujag <sidhujag@hotmail.com>
2016-10-13 10:24:36 -0700

    fix txrecord for ms inputs

--- src/qt/transactionrecord.cpp
+++ src/qt/transactionrecord.cpp
@@ -199,7 +199,8 @@ QList<TransactionRecord> TransactionRecord::decomposeTransaction(const CWallet *
 					sub.idx = parts.size(); // sequence number
 					sub.credit = txout.nValue;
 					sub.involvesWatchAddress = mine & ISMINE_WATCH_ONLY;
-					if (ExtractDestination(txout.scriptPubKey, address) && IsMine(*wallet, address))
+					// SYSCOIN
+					if (ExtractDestination(txout.scriptPubKey, address) /*&& IsMine(*wallet, address)*/)
 					{
 							// Received by Syscoin Address
 							sub.type = TransactionRecord::RecvWithAddress;
