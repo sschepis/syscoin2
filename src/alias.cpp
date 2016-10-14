ef02f15e776cb7bbefb6928c94f17c81b8b56d42
sidhujag <sidhujag@hotmail.com>
2016-10-14 11:53:59 -0700

    fix aliasnew test

--- src/alias.cpp
+++ src/alias.cpp
@@ -1758,6 +1758,7 @@ UniValue aliasnew(const UniValue& params, bool fHelp) {
 	SendMoneySyscoin(vecSend, recipient.nAmount + fee.nAmount, true, wtx);
 	UniValue res(UniValue::VARR);
 	res.push_back(wtx.GetHash().GetHex());
+	res.push_back(HexStr(vchPubKey));
 	return res;
 }
 UniValue aliasupdate(const UniValue& params, bool fHelp) {
