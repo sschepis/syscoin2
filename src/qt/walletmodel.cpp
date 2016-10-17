4891dc5d5b3c9c2f1331a925dcdf1a3d032da99b
sidhujag <sidhujag@hotmail.com>
2016-10-17 12:11:39 -0700

    Revert "include cc"
    This reverts commit 16f95da67848a0360c6777cb8e546e5430b0093d.

--- src/qt/walletmodel.cpp
+++ src/qt/walletmodel.cpp
@@ -28,7 +28,6 @@
 #include <boost/foreach.hpp>
 // SYSCOIN
 #include "guiutil.h"
-#include "coincontrol.h"
 #include "aliastablemodel.h"
 #include "messagetablemodel.h"
 #include "escrowtablemodel.h"
